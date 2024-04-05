9fb9dab8d08d88f4e0e51836f46c04d4fb784062
Tampham2-ptt <149355949+Tampham2-ptt@users.noreply.github.com>
2024-04-05 18:11:09 +0700

    add module types_model

--- contracts/models/contract_enterprise.py
+++ contracts/models/contract_enterprise.py
@@ -1,19 +1,22 @@
 from odoo import api, fields, models, tools, _
 from odoo.exceptions import UserError, ValidationError
+from datetime import datetime, timedelta
 
 class ContractEnterprise(models.Model):
     _name = "contract.enterprise"
     _description = "contract enterprise model"
 
-    id = fields.Integer('id', requests=True)
+    id = fields.Integer('id', required=True)
     name = fields.Char("contract name", required=True)
-    start_date = fields.Datetime('start date', requests=True, select=True)
-    end_date = fields.Datetime('end date', requests=True, select=True)
-    type_contract = fields.Integer('contract type', requests=True)
-    sign_day = fields.Datetime('sign day', requests=True, select=True)
-    salary_level = fields.Float('salary level', requests=False, default=0)
-    effective_salary = fields.Float('effective salary', requests=False, default=0)
+    start_date = fields.Datetime('start date', required=True, select=True, default=datetime.now())
+    end_date = fields.Datetime('end date', required=True, select=True)
+    type_contract = fields.Integer('contract type', required=True)
+    sign_day = fields.Datetime('sign day', required=True, select=True, default=datetime.now() + timedelta(days=7))
+    salary_level = fields.Float('salary level', required=False, default=0)
+    effective_salary = fields.Float('effective salary', required=False, default=0)
     status = fields.Selection([
-                ('complete', 'Complete'),
-                ('unfinished', 'Unfinished')
-            ], string='Status', default='unfinished')
+                ('new', 'New'),
+                ('running', 'Running'),
+                ('expired', 'Expired'),
+                ('pause', 'Pause')
+            ], string='Status', default='new')
